6f20defc68b407b1e465efffc9817fa308d55f32
Mikah Kainen <mikah@mltd.com>
2020-10-09 14:56:44 -0700

    started stufff

--- FirstAssignment/Source.cpp
+++ FirstAssignment/Source.cpp
@@ -3,18 +3,42 @@
 
 int main()
 {
-	using std::cout;
-	using std::cin;
-	std::cout << "test \n" << std::endl;
-	int a;
-	cin >> a;
+	//using std::cout;
+	//using std::cin;
+	//std::cout << "test \n" << std::endl;
+	//int a;
+	//cin >> a;
 
-	std::string myString;
+	//std::string myString;
 
-	cin >> myString;
+	//cin >> myString;
 
-	std::cout << "did run" << "\nnum: " << a << "\nstring: " << myString << std::endl;
+	//std::cout << "did run" << "\nnum: " << a << "\nstring: " << myString << std::endl;
 
-	std::cout << "\nline: " << myString;
+	//std::cout << "\nline: " << myString;
  	
+	std::cout << "You are playing Mad Libs, please enter when asked with proper grammar";
+
+	std::string noun;
+	std::string verb;
+	std::string adverb;
+	std::string noun2;
+	std::string noun3;
+	std::string verb2;
+
+	std::cout << "please give a noun";
+	std::cin >> noun2;
+	std::cout << "please give a noun";
+	std::cin >> noun3;
+	std::cout << "please give a noun";
+	std::cin >> noun;
+	std::cout << "please give a verb";
+	std::cin >> verb;
+	std::cout << "please give a verb";
+	std::cin >> verb2;
+	std::cout << "please give a adverb";
+	std::cin >> adverb;
+
+	std::cout << "\nThe " << noun << " \n " << adverb << " \n" << verb << " while the " << "\n" << noun2 << " \n " << verb2 << "the" << " \n " << noun3;
+
 }
